5a6a162bd451b40448c38c98bbe3a7fea110b41a
camelcrush <dkxm0927@naver.com>
2022-04-16 14:33:05 +0900

    Architecture with graphql-tools

---
+++ schema.js
@@ -0,0 +1,12 @@
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+const loadTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`);
+const typeDefs = mergeTypeDefs(loadTypes);
+const resolvers = mergeResolvers(loadResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
