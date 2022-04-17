a2056aff3c6e6532fb5e7944bc1b8a21ab59caab
camelcrush <dkxm0927@naver.com>
2022-04-17 11:42:07 +0900

    Login: jsonwebtoken

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bycrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client";
 
 export default {
@@ -29,6 +30,29 @@ export default {
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      // User Check
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      // Compare Password
+      const passwordOk = await bycrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Wrong password.",
+        };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.TOKEN_SECRET);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
