74205bf98da5ab9bc291a8e4686b1e2c87d94376
camelcrush <dkxm0927@naver.com>
2022-04-26 23:56:42 +0900

    computed Field: read field

---
+++ messages/messages.resolvers.js
@@ -0,0 +1,27 @@
+import client from "../client";
+
+export default {
+  Room: {
+    users: ({ id }) => client.room.findUnique({ where: { id } }).users(),
+    messages: ({ id }) =>
+      client.message.findMany({
+        where: { roomId: id },
+      }),
+    unreadTotal: ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return 0;
+      }
+      // 해당 방에 읽지 않은, 내가 보내지 않은 메세지 카운트
+      return client.message.count({
+        where: {
+          read: false,
+          roomId: id,
+          user: { id: { not: loggedInUser.id } },
+        },
+      });
+    },
+  },
+  Message: {
+    user: ({ id }) => client.message.findUnique({ where: { id } }).user(),
+  },
+};
