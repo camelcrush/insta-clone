a71bc72ccf2e336bd76963fc1ae8c02da04687f8
camelcrush <dkxm0927@naver.com>
2022-04-29 23:33:47 +0900

    Filtering subscription: withFilter(), currying fn

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,29 @@
+import { withFilter } from "graphql-subscriptions";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubSub";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: { id: args.id },
+          select: { id: true },
+        });
+        if (!room) {
+          // room이 존재하지 않으면 에러를 일으킨다.
+          // return null을 하게되면 sbuscribe는 함수를 리턴해야 하는데 null값으로 인해 에러 발생.
+          throw new Error("Room not found.");
+        }
+        // withFillter는 resolveFn이기 때문에 fx()()인 currying function으로 만들어서 함수를 call해줘야함.
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
